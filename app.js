c84dcf726df5331339068b6f8ed895673f34b9d8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-10-21 14:46:41 +0300

    changes made

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+let choices = ["rock","paper","scissor"]
+let randomNumber = Math.floor(Math.random() * choices.length)
+
+
+
+
+
+
+function getComputerChoice() {
+
+let compChoice = choices[randomNumber]
+return compChoice
+
+}
